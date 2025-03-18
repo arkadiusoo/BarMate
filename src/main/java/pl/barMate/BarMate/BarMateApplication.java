90f9c2e0a2f2189d1ede43f3757b9b0594a8b486
Kuba <janochajakub02@gmail.com>
2025-03-18 21:53:13 +0100

    zip unpack

---
+++ src/main/java/pl/barMate/BarMate/BarMateApplication.java
@@ -0,0 +1,13 @@
+package pl.barMate.BarMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/barMate/BarMate/BarMateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.barMate.BarMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
