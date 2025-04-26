cb4466425d2b2d57d4b3e8120a1eedb392f230bf
achojnacka <163404448+achojnacka@users.noreply.github.com>
2025-04-26 14:36:34 +0200

    dto

---
+++ shopping_service/src/main/java/pl/barMate/dto/ShoppingListDTO.java
@@ -0,0 +1,28 @@
+package pl.barMate.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Data transfer object representing a shopping list")
+public class ShoppingListDTO {
+
+    @Schema(description = "Unique identifier of the shopping list", example = "1")
+    private Long id;
+
+    @Schema(description = "Identifier of the user who created the shopping a list", example = "1")
+    private Long userId;
+
+    @Schema(description = "List of items on the shopping list", example = "[[1, 'Flour', 2, 'kg', 'true', 2]]")
+    private List<ShoppingListDTO> items;
+
+    @Schema(description = "Time of creation of a list", example = "2025-04-26")
+    private LocalDate createdAt;
+}
