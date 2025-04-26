d35472fd502496788e1a6b0d4f2d48de3b3c3d5b
achojnacka <163404448+achojnacka@users.noreply.github.com>
2025-04-26 14:58:45 +0200

    repositories

---
+++ shopping_service/src/main/java/pl/barMate/repository/ShoppingListRepository.java
@@ -0,0 +1,12 @@
+package pl.barMate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.barMate.model.ShoppingList;
+
+import java.util.List;
+
+@Repository
+public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+    List<ShoppingList> findByUserId(Long userId);
+}
