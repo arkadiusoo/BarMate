363bcca857dd27bfe6b55b2e163048ff5a7a466d
Kuba <janochajakub02@gmail.com>
2025-06-11 15:46:54 +0200

    Eureka server is working

---
+++ app/src/main/java/pl/summarizer/app/AppApplication.java
@@ -0,0 +1,17 @@
+package pl.summarizer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class})
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
