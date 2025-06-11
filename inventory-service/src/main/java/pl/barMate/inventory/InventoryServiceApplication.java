b08f47fb4919062b992696271596e45cfd92ac95
Kuba <janochajakub02@gmail.com>
2025-06-11 21:14:17 +0200

    Eureka!

--- inventory-service/src/main/java/pl/barMate/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/pl/barMate/inventory/InventoryServiceApplication.java
@@ -2,8 +2,9 @@ package pl.barMate.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryServiceApplication {
     public static void main(String[] args) {
