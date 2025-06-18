234c689c49c5c251c71b04ce46afb515c321f5d9
Kuba <janochajakub02@gmail.com>
2025-06-18 16:35:20 +0200

    name changes

--- user-service/src/main/java/pl/barMate/UserServiceApplication.java
+++ user-service/src/main/java/pl/barMate/UserServiceApplication.java
@@ -2,9 +2,10 @@ package pl.barMate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
