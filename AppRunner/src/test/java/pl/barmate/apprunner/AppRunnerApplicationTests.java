625130d9eb620133ddd85c4de2dbbd69369a648d
Kuba <janochajakub02@gmail.com>
2025-06-18 16:36:15 +0200

    name changes

---
+++ AppRunner/src/test/java/pl/barmate/apprunner/AppRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.barmate.apprunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
