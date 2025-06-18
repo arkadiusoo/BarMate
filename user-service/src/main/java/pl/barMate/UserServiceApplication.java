472679445bc849d5d4e99da74fa7ad8ceda6b921
szymskul <szymskul@gmail.com>
2025-06-18 16:19:31 +0200

    new

---
+++ user-service/src/main/java/pl/barMate/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.barMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
