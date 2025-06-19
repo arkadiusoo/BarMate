32d992b23036bcc787790f0ad87d0e6bde37353e
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-06-20 00:26:16 +0200

    cucumber development

--- analytics-service/src/test/java/pl/barmate/analyticsservice/behavioral/ChartGenerationSteps.java
+++ analytics-service/src/test/java/pl/barmate/analyticsservice/behavioral/ChartGenerationSteps.java
@@ -1,4 +1,37 @@
 package pl.barmate.analyticsservice.behavioral;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pl.barmate.analyticsservice.model.ChartType;
+import pl.barmate.analyticsservice.service.ChartService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@Component
 public class ChartGenerationSteps {
+
+    @Autowired
+    private ChartService chartService;
+
+    private Long userId;
+    private byte[] generatedChart;
+
+    @Given("a user with ID {int} exists")
+    public void aUserWithIdExists(int id) {
+        this.userId = (long) id;
+        // zakładamy, że użytkownik istnieje – setup w bazie lub mock w testach
+    }
+
+    @When("the user requests a chart for the most popular recipes")
+    public void theUserRequestsChartForMostPopularRecipes() {
+        this.generatedChart = chartService.generateChart(ChartType.TheMostPopularRecipies, userId);
+    }
+
+    @Then("the chart is generated successfully")
+    public void theChartIsGeneratedSuccessfully() {
+        assertThat(generatedChart).isNotNull();
+        assertThat(generatedChart.length).isGreaterThan(0);
+    }
 }
--- analytics-service/src/test/resources/features/chart_generation.feature
+++ analytics-service/src/test/resources/features/chart_generation.feature
@@ -1,6 +1,7 @@
-# Created by arkadiusz at 20/06/2025
-Feature: # Enter feature name here
-  # Enter feature description here
+# Created by arkadiusoo at 20/06/2025
+Feature: Chart generation
 
-  Scenario: # Enter scenario name here
-    # Enter steps here
+  Scenario: User generates a chart of most popular recipes
+    Given a user with ID 1 exists
+    When the user requests a chart for the most popular recipes
+    Then the chart is generated successfully
